6e3e492069477a00dce98829090578e44589f37d
Manokamna Singh <85088233+Manokamnasingh1@users.noreply.github.com>
2022-09-16 12:46:14 +0530

    second

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,include
+
+from home import views
+
+urlpatterns = [
+
+    path('', views.home, name ='home'),
+   
+
+]
---
+++ home/views.py
@@ -0,0 +1,8 @@
+
+from django.shortcuts import render
+from django.conf import settings
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
